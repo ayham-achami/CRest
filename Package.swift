be9ae372e7655b6735536bbf9540bad0e46f9875
Aleksandr Myaots <amyaots@gmail.com>
2022-11-07 13:56:23 +0300

    fix package deprecations

--- Package.swift
+++ Package.swift
@@ -14,9 +14,8 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-        .package(name: "Alamofire", url: "https://github.com/Alamofire/Alamofire.git", from: "5.6.2"),
-        .package(name: "CFoundation", url: "https://github.com/ayham-achami/CFoundation.git", .branch("mainline"))
+        .package(url: "https://github.com/Alamofire/Alamofire.git", from: .init(5, 6, 2)),
+        .package(url: "https://github.com/ayham-achami/CFoundation.git", branch: "mainline")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
