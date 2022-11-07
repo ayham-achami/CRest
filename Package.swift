3128cf925854c55cec3a5ee70164a316216b42d2
Aleksandr Myaots <amyaots@gmail.com>
2022-11-07 13:36:25 +0300

    edit package file

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "CRest",
-    platforms: [.iOS(.v13)],
+    platforms: [.iOS(.v13), .macCatalyst(.v13), .macOS(.v10_15)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
