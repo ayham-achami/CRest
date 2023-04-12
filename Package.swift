6713d82580c437bdf8bf7b5e21d845d2164a1cbe
Ayham Hylam <a.hylam@orbita.center>
2023-04-12 22:38:05 +0300

    fix development version for pod and SwiftPM

--- Package.swift
+++ Package.swift
@@ -5,7 +5,11 @@ import PackageDescription
 
 let package = Package(
     name: "CRest",
-    platforms: [.iOS(.v13), .macCatalyst(.v13), .macOS(.v10_15)],
+    platforms: [.iOS(.v13),
+                .tvOS(.v13),
+                .watchOS(.v6),
+                .macOS(.v10_15),
+                .macCatalyst(.v13)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
